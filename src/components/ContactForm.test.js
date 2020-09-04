b9772abab6903e87ce77472ee99b5ccf3c644a3e
Daniel Antonio <danieltomantonio@gamil.com>
2020-09-03 19:39:28 -0700

    Added IDs to inputs, queried inputs for ContactForm.test.js

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,3 +5,17 @@ import ContactForm from './ContactForm';
 test("Renders ContactForm without crashing", () => {
   render(<ContactForm />)
 });
+
+test("Can type information into Contact Form and Submit", () => {
+  render(<ContactForm />)
+// Type into input fields and click submit
+// 1.) Query for all inputs
+  const fistNameInput = screen.getByLabelText(/First Name/i);
+  const lastNameInput = screen.getByLabelText(/Last Name/i);
+  const emailInput = screen.getByLabelText(/Email/i);
+  const messageInput = screen.getByLabelText(/Message/i);
+// 2.) Run the change event to add text
+// Click Submit button
+// 1.) Query Submit button
+// 2.)
+})
