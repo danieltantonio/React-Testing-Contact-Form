0e3b3fe2bd37114361dd2933b24e2bd470593b88
Daniel Antonio <danieltomantonio@gamil.com>
2020-09-03 19:09:57 -0700

    Initialized ContactForm test

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ContactForm from './ContactForm';
+
+test("Renders ContactForm without crashing", () => {
+  render(<ContactForm />)
+});
